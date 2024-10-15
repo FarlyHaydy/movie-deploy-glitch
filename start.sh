5f22864768e6f690945f9cf72f2123d4537c9b79
Farlyhaydy H.Djalil <farlyhay1@gmail.com>
2024-10-16 03:38:29 +0800

    add start.sh

---
+++ start.sh
@@ -0,0 +1,15 @@
+
+VIRTUALENV=.data/venv
+
+if [ ! -d $VIRTUALENV ]; then
+  python3 -m venv $VIRTUALENV
+fi
+
+if [ ! -f $VIRTUALENV/bin/pip ]; then
+  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/pip/3.7/get-pip.py | $VIRTUALENV/bin/python
+fi
+
+$VIRTUALENV/bin/pip install -r requirements.txt
+
+$VIRTUALENV/bin/python3 app.py
+Footer
